4d7a16bae3af48b743de8aac0bd47406d7be4c18
Maik Römpagel <maik.roempagel@gmail.com>
2020-12-30 12:46:55 +0100

    Finish select a dashboard refresh interval lesson

--- lib/live_view_studio_web/live/sales_dashboard_live.ex
+++ lib/live_view_studio_web/live/sales_dashboard_live.ex
@@ -4,11 +4,17 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
   alias LiveViewStudio.Sales
 
   def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign_stats()
+      |> assign(:refresh, 1)
+      |> assign(:last_updated_at, Timex.now())
+
     if connected?(socket) do
-      :timer.send_interval(1000, self(), :tick)
+      schedule_refresh(socket)
     end
 
-    {:ok, assign_stats(socket)}
+    {:ok, socket}
   end
 
   def render(assigns) do
@@ -41,17 +47,45 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
             </span>
           </div>
         </div>
-        <button phx-click="refresh">
-          <img src="images/refresh.svg">
-          Refresh
-        </button>
+        <div class="controls">
+          <label for="refresh">
+            Refresh every:&nbsp;
+          </label>
+          <form phx-change="select-refresh">
+            <select name="refresh">
+              <%= options_for_select(refresh_options(), @refresh) %>
+            </select>
+          </form>
+          <button phx-click="refresh">
+            <img src="images/refresh.svg">
+            Refresh
+          </button>
+          <span>Last updated at: <%= Timex.format!(@last_updated_at, "%H:%M:%S", :strftime) %></span>
+        </div>
       </div>
     """
   end
 
   def handle_event("refresh", _, socket), do: {:noreply, assign_stats(socket)}
 
-  def handle_info(:tick, socket), do: {:noreply, assign_stats(socket)}
+  def handle_event("select-refresh", %{"refresh" => refresh}, socket) do
+    refresh = String.to_integer(refresh)
+
+    socket = assign(socket, refresh: refresh)
+
+    {:noreply, socket}
+  end
+
+  def handle_info(:tick, socket) do
+    socket =
+      socket
+      |> assign_stats()
+      |> assign(:last_updated_at, Timex.now())
+
+    schedule_refresh(socket)
+
+    {:noreply, socket}
+  end
 
   defp assign_stats(socket) do
     assign(socket,
@@ -59,5 +93,13 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
       sales_amount: Sales.sales_amount(),
       satisfaction: Sales.satisfaction()
     )
+  end
+
+  defp refresh_options do
+    [{"1s", 1}, {"5s", 5}, {"15s", 15}, {"30s", 30}, {"60s", 60}]
+  end
+
+  defp schedule_refresh(socket) do
+    Process.send_after(self(), :tick, socket.assigns.refresh * 1000)
   end
 end
