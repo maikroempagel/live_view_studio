b99f068626e69e552c36e5ecd77e11ec93cc94ac
Maik Römpagel <maik.roempagel@gmail.com>
2020-12-31 16:12:12 +0100

    Finish live navigation lesson

--- lib/live_view_studio_web/live/servers_live.ex
+++ lib/live_view_studio_web/live/servers_live.ex
@@ -15,6 +15,21 @@ defmodule LiveViewStudioWeb.ServersLive do
     {:ok, socket}
   end
 
+  def handle_params(%{"id" => id}, _url, socket) do
+    id = String.to_integer(id)
+    server = Servers.get_server!(id)
+
+    socket =
+      assign(socket,
+        selected_server: server,
+        page_title: "What's up #{server.name}?"
+      )
+
+    {:noreply, socket}
+  end
+
+  def handle_params(_, _url, socket), do: {:noreply, socket}
+
   def render(assigns) do
     ~L"""
     <h1>Servers</h1>
@@ -22,11 +37,13 @@ defmodule LiveViewStudioWeb.ServersLive do
       <div class="sidebar">
         <nav>
           <%= for server <- @servers do %>
-            <a href="#"
-               class="<%= if server == @selected_server, do: 'active' %>">
-              <img src="/images/server.svg">
-              <%= server.name %>
-            </a>
+            <div>
+              <%= live_patch link_body(server),
+                    to: Routes.live_path(@socket, __MODULE__, id: server.id),
+                    replace: true,
+                    class: if server == @selected_server, do: "active"
+              %>
+            </div>
           <% end %>
         </nav>
       </div>
@@ -70,6 +87,15 @@ defmodule LiveViewStudioWeb.ServersLive do
         </div>
       </div>
     </div>
+    """
+  end
+
+  def link_body(server) do
+    assigns = %{name: server.name}
+
+    ~L"""
+    <img src="images/server.svg">
+    <%= @name %>
     """
   end
 end
